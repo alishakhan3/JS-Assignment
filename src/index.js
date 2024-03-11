bff0de3d7b913a59d47487c2f4e7ac2ec5b6517a
alishakhan3 <alisha.khan@accolitedigital.com>
2024-03-11 21:17:50 +0530

    final

--- src/index.js
+++ src/index.js
@@ -13,10 +13,6 @@ var information = [
 ];
 
 function test() {
-    /*var fname=document.getElementById("fname")
-    var lname=document.getElementById("lname")
-    var department=document.getElementById("department")*/
-
     var fname = $('#fname')[0].value;
     var lname = $('#lname')[0].value;
     var gender = $('input[name=gender]:checked', '#myform').val();
@@ -40,42 +36,14 @@ function test() {
 
     information.push(student);
 
-    //$('input').css('background-color', 'grey')
-
-    //greetMe(fname.value + " " + lname.value); 
-    //when using this with jquery, we get undefined
-    /*greetMe(fname[0].value + " " + lname[0].value + " "+ gender + " " + age[0].value + " "+ 
-    + " " + rnumber[0].value + " "+ birthday[0].value);*/
-    //why is it iterable
-
     alert(`Hello ${fname} ${lname} (${department}, ${rnumber}, age: ${age}, ${gender}, ${birthday})`);//backtick
     alert(`Hobbies: ${hobbiesarray}`);
 };
-
-/*function showinfo(studentinfo) {
-    const result = $('#rsearch');
-    result.empty();
-    if (studentinfo) {
-        for (const [key, value] of Object.entries(studentinfo)) {
-            const newDiv = $('<div>').text(`${key} : ${value}`);
-            result.append(newDiv);
-        }
-    } else {
-        const newDiv = $('<div>').text(`Invalid Roll Number`);
-        result.append(newDiv);
-    }
-}*/
 
 function store() {
     var rnumber = parseInt($('#rnumber').val());
 
     const studentinfo = information.find(student => student.rnumber == rnumber);
-    //showinfo(studentinfo);
-    /*var studentinfo=information.find(function (s) { //anonymous function
-        return s.student.rnumber==rnumber;
-    });*/
-
-    //alert(`${studentinfo}`);
 
     if(studentinfo){
         alert(`${studentinfo.fname} ${studentinfo.lname}`);
